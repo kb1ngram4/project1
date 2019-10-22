6daee8ee298cead33c0b3f4e884ea946f917eb38
Lonzo <1318947374@qq.com>
2019-10-22 19:03:03 +0800

    开发环境的搭建

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
-import "@babel/polyfill"
+//开发环境不需要polyfill,生产环境再打开
+// import "@babel/polyfill" 
 import "./business/about"
 import "./business/course"
 import "./business/home"
 import "./business/team"
 import "./business/works"
 import "./frame/frame"
-setTimeout(() => {
-    console.log(Object.assign({},{name:"kone"},{age:18}));
-    
-}, 1000);
